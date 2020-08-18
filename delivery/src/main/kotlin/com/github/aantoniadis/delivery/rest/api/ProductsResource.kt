beed84ad36668909b91bd33fe44e2080663f116d
madirysbek <hooizit@gmail.com>
2020-08-18 22:01:34 +0600

    rest controller mapping

--- delivery/src/main/kotlin/com/github/aantoniadis/delivery/rest/api/ProductsResource.kt
+++ delivery/src/main/kotlin/com/github/aantoniadis/delivery/rest/api/ProductsResource.kt
@@ -7,9 +7,24 @@ import java.util.concurrent.CompletionStage
 @RestController
 @RequestMapping("/products")
 interface ProductsResource {
+    @GetMapping("/all")
+    fun getAllProducts() {
+        println("get all products list")
+    }
+
     @GetMapping("/{code}")
     fun getProductByCode(@PathVariable("code") code: String): CompletionStage<ProductDto>
 
-    @PostMapping
+    @PostMapping("/add")
     fun createProduct(@RequestBody productDto: ProductDto): CompletionStage<ResponseEntity<Unit>>
+
+    @DeleteMapping("/{code}")
+    fun deleteProductByCode(@PathVariable("code") code: String) {
+        println("delete product $code")
+    }
+
+    @PutMapping("/{code}")
+    fun updateProductByCode(@PathVariable("code") code: String) {
+        println("update product $code")
+    }
 }
